5cb0dace588128c1389d8fd859bb4b6e680c5cef
Shriram Rajeshwar Nawathe <108632713+ShriramNawathe@users.noreply.github.com>
2023-03-28 20:04:49 +0530

    sol 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const ProgressBar = () => {
+  const [progress, setProgress] = useState(0);
 
-const App = () => {
+  useEffect(() => {
+    // Asynchronous operation that updates the progress
+    const asyncOperation = async () => {
+      for (let i = 0; i < 100; i++) {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        setProgress(i + 1);
+      }
+    };
+
+    asyncOperation();
+  }, []);
+
+  const barInnerStyle = {
+    width: `${progress}%`,
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="barOuter">
+      <div className="title" >
+        <h1>Progress Bar</h1>
+      </div>
+      <div id="barInner" style={barInnerStyle}>
+        
+      </div>
+      <br />
+      <div>
+      {`${progress}%`}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default ProgressBar;
